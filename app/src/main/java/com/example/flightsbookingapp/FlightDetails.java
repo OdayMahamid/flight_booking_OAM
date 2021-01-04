246c9b76bbca5e35109fe305755270d5edee3744
AseelNemer <AseelNemer@users.noreply.github.com>
2021-01-04 14:29:06 +0200

    manager update!

--- app/src/main/java/com/example/flightsbookingapp/FlightDetails.java
+++ app/src/main/java/com/example/flightsbookingapp/FlightDetails.java
@@ -144,6 +144,8 @@ public class FlightDetails extends AppCompatActivity implements View.OnClickList
             }
         });
     }
+
+
     @Override
     public void onClick(View v) { }
 
--- app/src/main/java/com/example/flightsbookingapp/manager_profile.java
+++ app/src/main/java/com/example/flightsbookingapp/manager_profile.java
@@ -1,11 +1,13 @@
 package com.example.flightsbookingapp;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,9 +21,17 @@ import com.google.firebase.database.ValueEventListener;
 
 public class manager_profile extends AppCompatActivity implements View.OnClickListener{
     String manager_id;
-    private EditText name;
-    private EditText password;
+    private TextView name;
+    private TextView password;
     private TextView email;
+    private Button change_name;
+    private Button change_pass;
+    Button dialog_name;
+    EditText dialogname;
+    EditText dialogpass;
+    EditText dialogconfirm;
+    Button dialog_pass;
+    Dialog d,d1;
     ImageButton back;
     FirebaseDatabase db;
     DatabaseReference manageref;
@@ -32,10 +42,12 @@ public class manager_profile extends AppCompatActivity implements View.OnClickLi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_managerprofile);
         back=findViewById(R.id.back_button);
-        name=findViewById(R.id.name_text);
-        password=findViewById(R.id.password_text);
+        name=findViewById(R.id.nametext);
+        password=findViewById(R.id.passtext);
         email=findViewById(R.id.email_text);
         manager_id=getIntent().getStringExtra("manager_id");
+        change_name=findViewById(R.id.namechange);
+        change_pass=findViewById(R.id.passwordchange);
 
         db=FirebaseDatabase.getInstance();
         manageref=db.getReference("Managers").child(manager_id);
@@ -43,7 +55,6 @@ public class manager_profile extends AppCompatActivity implements View.OnClickLi
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
              Manager m = snapshot.getValue(Manager.class);
-
 //                name.setText(snapshot.child("name").getValue().toString());
 //                email.setText(snapshot.child("email").getValue().toString());
 //                password.setText(snapshot.child("password").getValue().toString());
@@ -57,12 +68,76 @@ public class manager_profile extends AppCompatActivity implements View.OnClickLi
 
             }
         });
+        change_pass.setOnClickListener(this);
+        change_name.setOnClickListener(this);
+
+
+    }
+    public  void changename()
+    {
+        d=new Dialog(this);
+        d.setContentView(R.layout.layout_custom);
+        d.setTitle("change your name");
+        d.setCancelable(true);
+        dialogname=d.findViewById(R.id.dialognametext);
+        dialog_name=d.findViewById(R.id.change_button);
+        dialog_name.setOnClickListener(this);
+        d.show();
+
+    }
+    public void changepassword()
+    {
+        d1=new Dialog(this);
+        d1.setContentView(R.layout.change_password);
+        d1.setTitle("change your password");
+        d1.setCancelable(true);
+        dialogpass=d1.findViewById(R.id.newpass);
+        dialogconfirm=d1.findViewById(R.id.confirmpass);
+        dialog_pass=d1.findViewById(R.id.pass_change_b);
+        dialog_pass.setOnClickListener(this);
+        d1.show();
 
 
     }
 
     @Override
     public void onClick(View v) {
+        if(v.getId() == R.id.namechange)
+        {
+            changename();
+
+        }
+        if(v.getId() == R.id.passwordchange)
+        {
+            changepassword();
+        }
+        if(v.getId() == R.id.change_button)
+        {
+            String newname=dialogname.getText().toString();
+            if(newname!=null)
+            {
+                manageref.child("name").setValue(newname);
+                Toast.makeText(this,"user name changed ",Toast.LENGTH_LONG).show();
+                d.dismiss();
+
+            }
+        }
+        if(v.getId() == R.id.pass_change_b)
+        {
+            String pass=dialogpass.getText().toString().trim();
+            String conf=dialogconfirm.getText().toString().trim();
+            if(pass.equals(conf))
+            {
+                manageref.child("password").setValue(pass);
+                System.out.println(pass);
+                System.out.println(conf);
+                Toast.makeText(this,"user password changed ",Toast.LENGTH_LONG).show();
+            }
+            else
+                Toast.makeText(this,"password and confirm password should match! ",Toast.LENGTH_LONG).show();
+
+            d1.dismiss();
+        }
 
     }
 }
